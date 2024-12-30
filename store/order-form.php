fe2033c4113611f3bd35c1d32db91c9c6f0a99a4
Nathaniel Campbell <nixc.web@gmail.com>
2024-12-30 16:54:07 -0600

    Add MySQL db connection
    Note: config is hidden for security

--- store/order-form.php
+++ store/order-form.php
@@ -1,36 +1,39 @@
 <?php
-  // Page variables
-  $page_title = "Order Form";
+  include 'config/database.php';
+  $page_title = "Order Form";   // Page variables
 
+  // Database connection
+  $sql = "SELECT * FROM products";
+  $result = mysqli_query($conn, $sql);
+  $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
   // Spoof DB 🤡
   // Product class
-  class product {
-    public $name;
-    public $price;
-    public $description;
-    public $image;
-    public $id;
+  // class product {
+  //   public $name;
+  //   public $price;
+  //   public $description;
+  //   public $image;
+  //   public $id;
 
-    public function __construct($name, $price, $description, $image, $id) {
-      $this->name = $name;
-      $this->price = $price;
-      $this->description = $description;
-      $this->image = $image;
-      $this->id = $id;
-    }
-  }
+  //   public function __construct($name, $price, $description, $image, $id) {
+  //     $this->name = $name;
+  //     $this->price = $price;
+  //     $this->description = $description;
+  //     $this->image = $image;
+  //     $this->id = $id;
+  //   }
+  // }
 
 
   // Array of 5 products
-  $products = [
-    new product('Chair Pads', 75.00, 'A chair pad', '', 1),
-    new product('Laptop', 215, 'A refurbished laptop with Quizstuff installed.', '', 2),
-    new product('USB Interface', 135.00, 'ProductDescription', '', 3),
-    new product('QuizMachine', 50, 'ProductDescription', '', 4),
-    new product('QuizMachine DQD', 75, 'ProductDescription', '', 5)
-  ];
+  // $products = [
+  //   new product('Chair Pads', 75.00, 'A chair pad', '', 1),
+  //   new product('Laptop', 215, 'A refurbished laptop with Quizstuff installed.', '', 2),
+  //   new product('USB Interface', 135.00, 'ProductDescription', '', 3),
+  //   new product('QuizMachine', 50, 'ProductDescription', '', 4),
+  //   new product('QuizMachine DQD', 75, 'ProductDescription', '', 5)
+  // ];
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
   <?php include '../head.php'; ?>
@@ -44,16 +47,15 @@
       <section class="container">
       <h1>Order Form</h1>
         <form>
-          <?php 
-            foreach ($products as $product) {
-              echo "<fieldset>";
-              echo "<legend>$product->name</legend>";
-              echo "<label>Price: $$product->price</label>";
-              // echo "<label>Description: $product->description</label>";
-              echo "<label>Quantity: <input type='number' name='quantity' id='quantity' min='0' max='100' steps='1' value='0'></label>";
-              echo "</fieldset>";
-            }
-          ?>
+          <?php foreach($products as $product) : ?>
+            <fieldset>
+              <legend><?= $product['name'] ?></legend>
+              <label>Price: $<?= $product['price'] ?></label>
+              <label>Description: <?= $product['description'] ?></label>
+              <img src="..<?= $product['image'] ?>" alt="<?= $product['name'] ?>">
+              <label>Quantity: <input type="number" name="quantity_<?= $product['id'] ?>" id="quantity_<?= $product['id'] ?>" min="0" max="100" steps="1" value="0"></label>
+            </fieldset>
+          <?php endforeach; ?>
           <button>Submit Order</button>
         </form>
       </section>
