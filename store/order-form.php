bd3f94b6c587c056052802b3215c1ef328605af6
Nathaniel Campbell <nixc.web@gmail.com>
2025-01-09 09:59:53 -0600

    remove form attributes to avoid confusion

--- store/order-form.php
+++ store/order-form.php
@@ -61,7 +61,7 @@
       <section class="container">
       <h1>Order Form</h1>
       <p>Fill out the form below to place an order.</p>
-        <form id="order-form" action="../order.php" method="POST" enctype="multipart/form-data">
+        <form id="order-form">
           <h2>Customer Information</h2>
           <fieldset>
             <legend>Name</legend>
