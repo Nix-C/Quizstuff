f0d974420e2a1498661c4660495e1184d884059b
Michael Parenti <mparenti677@gmail.com>
2025-01-15 16:29:02 -0600

    Print basic invoice

---
+++ store/send-user-invoice.php
@@ -0,0 +1,27 @@
+<?php
+
+include 'generate-invoice.php';
+
+function sendUserInvoice(){
+
+    //Use HTML Variable
+    ob_start();
+?>
+
+
+
+<body>
+    <h1>Quizstuff Invoice</h1>
+    <?php $invoice ?>
+</body>
+
+
+
+<?php
+
+    //This is the name of the HTML Variable
+    $userEmail = ob_get_clean();
+
+}
+
+?>
