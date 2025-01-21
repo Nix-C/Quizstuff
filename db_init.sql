7567b942cd14459b93b465a66a8e5e3e859d6ddd
Nathaniel Campbell <nixc.web@gmail.com>
2025-01-21 12:44:20 -0600

    Update db_init.sql

--- db_init.sql
+++ db_init.sql
@@ -1,4 +1,4 @@
-/* Create the database and tables for the QuizStuff application */
+/* Create the database and tables for the Quizstuff application */
 CREATE DATABASE `quizstuff`;
 
 /* Product Table */
@@ -10,17 +10,19 @@ CREATE TABLE `quizstuff`.`products`(
   `has_options` BOOLEAN NOT NULL DEFAULT FALSE COMMENT 'Does this product have options?',
   `price` DECIMAL(10, 2) NOT NULL DEFAULT 0.00 COMMENT 'Price of product',
   `weight` DECIMAL(10, 2) NOT NULL DEFAULT 0.00 COMMENT 'Weight in pounds (lbs)',
+  `flat_shipping` DECIMAL(10,2) NULL,
   `description` VARCHAR(1000) NOT NULL DEFAULT '' COMMENT 'Description of product',
   `image` VARCHAR(255) NULL COMMENT 'Image URL',
   PRIMARY KEY(`id`)
 ) ENGINE = InnoDB COMMENT = 'Product details';
 
 /* Insert core products */
-INSERT INTO `quizstuff`.`products` (`name`, `model`, `has_variants`, `has_options`, `price`, `weight`, `image`, `description`) VALUES 
-('USB Interface Box', 'v0.9', FALSE, FALSE, 135.00, 0.00,'', 'USB Interface Box connects up to 3 strings of pads to a computer.\nUse QuizMachine software on Microsoft operating systems. Windows 7,8,10 all versions only.\nNo power cord required. Power is supplied via the USB port on the computer.'),
-('Chair Pads (5 Per String)', 'v18', TRUE, FALSE, 75.00, 0.00, '', 'NOTICE: Pads are being made as quickly as possible (within a 4-6 weeks provided parts are available) as another person is helping. Please contact me for more information.\nPads - strings of 5 pads. Colors: Red, Blue (old yellow) and Green.\nThese are intended to work with QuizMachine and either the USB or Parallel port interface boxes from QuizStuff, but we cannot necessarily guarantee the strings to work with other quiz equipment. Use with other equipment at your own risk.\nMade with Naugahyde leather and all sides sewn together professionally.'),
-('QuizMachine (1 User)', 'v5.4.J30', FALSE, TRUE, 50.00, 0.00, '', 'The software called QuizMachine used in Nazarene style bible quizzing. Detects which person jumps, keeps score, records all options for the rounds.\nScores can be exported to QMServer (included) to tally overall quiz stats.\nScoresheets can be printed.'),
-('QuizMachine DQD (5 Users)', 'v5.4.J30', FALSE, FALSE, 200.00, 0.00, '', 'DISTRICT DIRECTORS ONLY!\n5 registrations - includes lifetime upgrades.\nThe software called QuizMachine used in Nazarene style bible quizzing. Detects which person jumps, keeps score, records all options for the rounds.\nScores can be exported to QMServer (included) to tally overall quiz stats.\nScoresheets can be printed.');
+INSERT INTO `quizstuff`.`products` (`name`, `model`, `has_variants`, `has_options`, `price`, `weight`, `image`, `description`, `flat_shipping`) VALUES 
+('Quizstuff Hardware Bundle', NULL, FALSE, FALSE, 360.00, 0.00, NULL, 'Includes one USB Interface Box and three strings of Chair Pads. Flat rate of $15.95 shipping via USPS.', 15.95 ),
+('USB Interface Box', 'v0.9', FALSE, FALSE, 135.00, 0.00, NULL, 'USB Interface Box connects up to 3 strings of pads to a computer.\nUse QuizMachine software on Microsoft operating systems. Windows 7,8,10 all versions only.\nNo power cord required. Power is supplied via the USB port on the computer.', NULL),
+('Chair Pads (5 Per String)', 'v18', TRUE, FALSE, 75.00, 0.00, NULL, 'NOTICE: Pads are being made as quickly as possible (within a 4-6 weeks provided parts are available) as another person is helping. Please contact me for more information.\nPads - strings of 5 pads. Colors: Red, Blue (old yellow) and Green.\nThese are intended to work with QuizMachine and either the USB or Parallel port interface boxes from Quizstuff, but we cannot necessarily guarantee the strings to work with other quiz equipment. Use with other equipment at your own risk.\nMade with Naugahyde leather and all sides sewn together professionally.', NULL),
+('QuizMachine (1 User)', 'v5.4.J30', FALSE, FALSE, 75.00, 0.00, NULL, 'Single registration - includes lifetime upgrades.\nThe software called QuizMachine used in Nazarene style bible quizzing. Detects which person jumps, keeps score, records all options for the rounds.\nScores can be exported to QMServer (included) to tally overall quiz stats.\nScoresheets can be printed.', NULL),
+('QuizMachine DQD (5 Users)', 'v5.4.J30', FALSE, FALSE, 75.00, 0.00, NULL, 'DISTRICT DIRECTORS ONLY!\n5 registrations - includes lifetime upgrades.\nThe software called QuizMachine used in Nazarene style bible quizzing. Detects which person jumps, keeps score, records all options for the rounds.\nScores can be exported to QMServer (included) to tally overall quiz stats.\nScoresheets can be printed.', NULL);
 
 /* Product Variants */
 CREATE TABLE `quizstuff`.`product_variants`(
@@ -55,8 +57,6 @@ CREATE TABLE `quizstuff`.`product_options`(
 ) ENGINE = InnoDB COMMENT = 'Product option details';
 
 /* Insert product options */
-INSERT INTO `quizstuff`.`product_options` (`product_id`,`product_reference_id`, `name`, `price`, `description`) VALUES
-(3, NULL, 'Lifetime Upgrades', 25.00, 'Get lifetime upgrades for QuizMachine software.');
 
 /* Orders */
 CREATE TABLE `quizstuff`.`orders`(
@@ -79,8 +79,8 @@ CREATE TABLE `quizstuff`.`orders`(
 ) ENGINE = InnoDB COMMENT = 'Orders';
 
 /* Insert sample order */
-INSERT INTO `quizstuff`.`orders` (`name_first`, `name_last`, `address_1`, `address_2`, `city`, `state`, `zip`, `phone`, `email`, `total_price`)
-VALUES ('John', 'Doe', '123 Main St', 'Apt 4B', 'Anytown', 'NY', '12345', '1234567890', 'john.doe@example.com', 435.00);
+/* INSERT INTO `quizstuff`.`orders` (`name_first`, `name_last`, `address_1`, `address_2`, `city`, `state`, `zip`, `phone`, `email`, `total_price`)
+VALUES ('John', 'Doe', '123 Main St', 'Apt 4B', 'Anytown', 'NY', '12345', '1234567890', 'john.doe@example.com', 435.00); */
 
 /* Order Items */
 CREATE TABLE `quizstuff`.`order_items`(
@@ -100,9 +100,6 @@ CREATE TABLE `quizstuff`.`order_items`(
 ) ENGINE = InnoDB COMMENT = 'Order items; Get by order_id';
 
 /* Insert sample order items */
-INSERT INTO `quizstuff`.`order_items` (`order_id`, `product_id`, `variant_id`, `option_id`, `quantity`, `price`, `total_price`) VALUES 
-(1, 1, NULL, NULL, 1, 135.00, 135.00),
-(1, 2, 1, NULL, 2, 75.00, 150.00),
-(1, 2, 3, NULL, 1, 75.00, 75.00),
-(1, 3, NULL, NULL, 1, 50.00, 50.00),
-(1, 3, NULL, 1, 1, 25.00, 25.00);
+/* INSERT INTO `quizstuff`.`order_items` (`order_id`, `product_id`, `variant_id`, `option_id`, `quantity`, `price`, `total_price`) VALUES 
+	!! Values here !!
+ */
