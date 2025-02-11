c3b08b8defbe413cee2afb5ecb63ab74e6bf35e0
Nathaniel Campbell <nixc.web@gmail.com>
2025-02-11 15:02:04 -0600

    Download asset exe instead of zipball

--- listen.php
+++ listen.php
@@ -4,19 +4,36 @@
   if($payload){
     require_once 'config.php';
     $json_paylod = json_decode($payload);
-    $secret = QM_HOOK_SECRET;
+    // Verify signature
+    $secret = GH_HOOK_RELEASE_SECRET;
     $github_signature =  $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
     $expected_signature = 'sha256=' . hash_hmac('sha256', $payload, $secret);
-  
-    if (hash_equals($expected_signature, $github_signature)){
-      if(
-        $_SERVER['HTTP_X_GITHUB_EVENT'] === "release" 
-        && $json_paylod->action === "released"
-      ){
 
-        # Download from github using the api endpoint
+    // React to "released"
+    if (
+      $_SERVER['HTTP_X_GITHUB_EVENT'] === "release" 
+      && $json_paylod->action === "released"
+    ) {
+      // If signature good, do stuff
+      if(hash_equals($expected_signature, $github_signature)){
+        // Find first .exe available in assets
+        $assets = $json_paylod->release->assets;
+        $targetAsset = null;
+        foreach ($assets as $asset) {
+          if(str_ends_with($asset->name, ".exe")){
+            $targetAsset = $asset;
+            break;
+          }
+        }
+        if(!$targetAsset){
+          $OK = FALSE;
+          $error = "No assets in release.";
+          file_put_contents("logs.txt", "ERROR: " . $error, FILE_APPEND);
+        }
+
+        // Download from github using the api endpoint
         $token = GH_API_TOKEN;
-        $url = $json_paylod->release->zipball_url;
+        $url = $targetAsset->url;
         $headers = [
           "X-GitHub-Api-Version: 2022-11-28",
           "Authorization: ${token}",
@@ -28,29 +45,27 @@
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
         $response = curl_exec($ch);
+
         if (curl_errno($ch)) {
+          $OK = FALSE;
           $errorMessage = date('Y-m-d H:i:s') . " - cURL Error: " . curl_error($ch) . "\n";
-          file_put_contents("logs.txt", $errorMessage, FILE_APPEND);
+          file_put_contents("logs.txt", "ERROR: " . $errorMessage, FILE_APPEND);
         } else {
-          $tagName = $json_paylod->release->tag_name;
-
           // Update qm-version.json
-          $fileLocation = "/downloads/" . $tagName . ".zip";
+          $tagName = $json_paylod->release->tag_name;
+          $fileLocation = "/assets/downloads/" . $targetAsset->name;
+        
+          file_put_contents("qm-version.json", '{"version": "'.$tagName.'", "asset_url": "'.$fileLocation.'"}');
           
-
-          file_put_contents("qm-version.json", '
-{
-  "version": "'.$tagName.'",
-  "asset_url": "'.$fileLocation.'"
-}');
           // Put file
-          file_put_contents("./assets/downloads/" . $tagName . ".zip", $response);
-
+          file_put_contents("." . $fileLocation, $response);
         }
         
         curl_close($ch);
       }
       
+    // } else if (false){
+    //   # Do auto deployment here later
     } else {
       $OK = FALSE;
     }
