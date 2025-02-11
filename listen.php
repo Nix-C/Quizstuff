f9b0846a194dafaaa56a3060ba4ce261f8818b7a
Nathaniel Campbell <nixc.web@gmail.com>
2025-02-11 13:58:26 -0600

    Add webhook listener for QM releases

--- listen.php
+++ listen.php
@@ -4,7 +4,6 @@
   if($payload){
     require_once 'config.php';
     $json_paylod = json_decode($payload);
-
     $secret = QM_HOOK_SECRET;
     $github_signature =  $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
     $expected_signature = 'sha256=' . hash_hmac('sha256', $payload, $secret);
@@ -14,14 +13,45 @@
         $_SERVER['HTTP_X_GITHUB_EVENT'] === "release" 
         && $json_paylod->action === "released"
       ){
-        echo "Retrieving latest release files";
-        # Download from github using the Get a Release api endpoint
-        # https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#get-a-release
-  
+
+        # Download from github using the api endpoint
+        $token = GH_API_TOKEN;
+        $url = $json_paylod->release->zipball_url;
+        $headers = [
+          "X-GitHub-Api-Version: 2022-11-28",
+          "Authorization: ${token}",
+          "User-Agent: MyGitHubClient" // GitHub API requires a User-Agent header
+        ];
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        $response = curl_exec($ch);
+        if (curl_errno($ch)) {
+          $errorMessage = date('Y-m-d H:i:s') . " - cURL Error: " . curl_error($ch) . "\n";
+          file_put_contents("logs.txt", $errorMessage, FILE_APPEND);
+        } else {
+          $tagName = $json_paylod->release->tag_name;
+
+          // Update qm-version.json
+          $fileLocation = "/downloads/" . $tagName . ".zip";
+          
+
+          file_put_contents("qm-version.json", '
+{
+  "version": "'.$tagName.'",
+  "asset_url": "'.$fileLocation.'"
+}');
+          // Put file
+          file_put_contents("./assets/downloads/" . $tagName . ".zip", $response);
+
+        }
+        
+        curl_close($ch);
       }
       
     } else {
-      echo $json->hook->config->secret;
       $OK = FALSE;
     }
   } else {
