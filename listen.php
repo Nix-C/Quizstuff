8f8d439c3f91dcb121f73d5ae5afae23e6ed1123
Nathaniel Campbell <nixc.web@gmail.com>
2025-02-19 18:23:00 -0600

    Update logic to detect installer type

--- listen.php
+++ listen.php
@@ -16,66 +16,115 @@
     ) {
       // If signature good, do stuff
       if(hash_equals($expected_signature, $github_signature)){
-        // Find first .exe available in assets
+        // Loop through assets available
         $assets = $json_paylod->release->assets;
-        $targetAsset = null;
+        $installerData = [];
         foreach ($assets as $asset) {
-          if(str_ends_with($asset->name, ".exe")){
-            $targetAsset = $asset;
-            break;
+          if(str_ends_with($asset->name, ".exe") || str_ends_with($asset->name, ".deb")) {
+            // Download asset
+            ['assetPath' => $assetPath, 'sha1' => $sha1Value] = downloadAsset($asset);
+            
+            if($assetPath != null) {
+              /**
+               * We offer 4 versions:
+               * - Windows 10, 11
+               * - Windows 7, 8
+               * - Linux Intel/AMD Ubuntu
+               * - Linux Raspberry Pi
+               */
+
+              if(
+                str_ends_with($asset->name, ".exe")
+                && preg_match('/Installer(.*?)\.exe/i', $asset->name, $matches)
+              ){ // Windows 10, 11
+                $os = "windows";
+                $arch = $matches[1] ? $matches[1] : "unrecognized"; # TODO: UPDATE THIS!
+              } else if (
+                str_ends_with($asset->name, ".deb")
+                && preg_match('/-([^-.]+)\.deb/i', $asset->name, $matches)
+              ) { // Linux AMD/Intel Ubuntu
+                $os = "linux";
+                $arch = $matches[1] ? $matches[1] : "unrecognized";
+              } else {
+                $arch = "unknown";
+                $os = "unknown";
+                $OK = FALSE;
+                echo "ERROR: Unknown installer: " . $asset->name;
+              }
+              
+              // Push to installers
+              array_push($installerData, (object) [
+                'architecture' => $arch,
+                'os' => $os,
+                'sha1sum' => $sha1Value,
+                'url' => $assetPath
+              ]);
+
+
+            } else {
+              $OK = FALSE;
+              echo "ERROR: There was an issue with downloading the asset.";
+            }
+
           }
+          // Put json data
+          file_put_contents("qm-version.json", '{ "installers": ' . json_encode($installerData) . '}');
         }
-        if(!$targetAsset){
-          $OK = FALSE;
-          $error = "No assets in release.";
-          file_put_contents("logs.txt", "ERROR: " . $error, FILE_APPEND);
-        }
-
-        // Download from github using the api endpoint
-        $token = GH_API_TOKEN;
-        $url = $targetAsset->url;
-        $headers = [
-          "X-GitHub-Api-Version: 2022-11-28",
-          "Authorization: ${token}",
-          "User-Agent: MyGitHubClient" // GitHub API requires a User-Agent header
-        ];
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-        $response = curl_exec($ch);
-
-        if (curl_errno($ch)) {
-          $OK = FALSE;
-          $errorMessage = date('Y-m-d H:i:s') . " - cURL Error: " . curl_error($ch) . "\n";
-          file_put_contents("logs.txt", "ERROR: " . $errorMessage, FILE_APPEND);
-        } else {
-          // Update qm-version.json
-          $tagName = $json_paylod->release->tag_name;
-          $fileLocation = "/assets/downloads/" . $targetAsset->name;
-        
-          file_put_contents("qm-version.json", '{"version": "'.$tagName.'", "asset_url": "'.$fileLocation.'"}');
-          
-          // Put file
-          file_put_contents("." . $fileLocation, $response);
-        }
-        
-        curl_close($ch);
+      } else {
+        $OK = FALSE;
+        echo "ERROR: Incorrect hash.";
       }
       
     // } else if (false){
     //   # Do auto deployment here later
     } else {
       $OK = FALSE;
+      echo "ERROR: Unhandled or unrecognized event.";
     }
   } else {
     $OK = FALSE;
+    echo "Unable to read webhook payload.";
   }
 
   if($OK){
     http_response_code(200); // OK
   } else {
     http_response_code(404); // Bad request
+  }
+
+  function downloadAsset($asset){
+    // Download from github using the api endpoint
+    $token = GH_API_TOKEN;
+    $headers = [
+      "X-GitHub-Api-Version: 2022-11-28",
+      "Authorization: ${token}",
+      "User-Agent: MyGitHubClient" // GitHub API requires a User-Agent header
+    ];
+    $ch = curl_init($asset->url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+    $response = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+      $OK = FALSE;
+      $errorMessage = date('Y-m-d H:i:s') . " - cURL Error: " . curl_error($ch) . "\n";
+      file_put_contents("logs.txt", "ERROR: " . $errorMessage, FILE_APPEND);
+      curl_close($ch);
+    } else {
+      $assetPath = "/installers/" . $asset->name;
+      // Put file
+      file_put_contents("." . $assetPath, $response); 
+
+      // Get sha1 sum
+      $sha1Value = hash_file('sha1', "." . $assetPath);
+
+      curl_close($ch);
+      return [
+        'assetPath' => $assetPath,
+        'sha1' => $sha1Value
+      ];
+    }
   }
 ?>
