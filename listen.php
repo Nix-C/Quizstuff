6c39cd69efdd84d4c68333e2610dc7da420222a4
Nathaniel Campbell <nixc.web@gmail.com>
2025-02-20 12:20:51 -0600

    Finalize listen.php

--- listen.php
+++ listen.php
@@ -1,8 +1,49 @@
-<?php 
+<?php
+  require_once 'config.php';
   $OK = TRUE;
   $payload = file_get_contents('php://input') ?? null;
+
+  // Download asset and return sha1 string.
+  function downloadAsset($asset){
+    // Download from github using the api endpoint
+    $token = GH_API_TOKEN;
+    
+    $headers = [
+      "X-GitHub-Api-Version: 2022-11-28",
+      "Authorization: Bearer ${token}",
+      "Accept: application/octet-stream", // Tells GitHub to send the raw binary
+      "User-Agent: MyGitHubClient" // GitHub API requires a User-Agent header
+    ];
+
+    $ch = curl_init($asset->url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+    $response = curl_exec($ch);
+    if (curl_errno($ch)) {
+      $OK = FALSE;
+      $errorMessage = date('Y-m-d H:i:s') . " - cURL Error: " . curl_error($ch) . "\n";
+      echo $errorMessage . "\n";
+      file_put_contents("logs.txt", "ERROR: " . $errorMessage, FILE_APPEND);
+      curl_close($ch);
+    } else {
+      $assetPath = "/installers/" . $asset->name;
+      // Put file
+      file_put_contents("." . $assetPath, $response); 
+
+      // Get sha1 sum
+      $sha1Value = hash_file('sha1', "." . $assetPath);
+
+      curl_close($ch);
+      return [
+        'assetPath' => $assetPath,
+        'sha1' => $sha1Value
+      ];
+    }
+  }
+
   if($payload){
-    require_once 'config.php';
     $json_paylod = json_decode($payload);
     // Verify signature
     $secret = GH_HOOK_RELEASE_SECRET;
@@ -35,7 +76,7 @@
 
               if(
                 str_ends_with($asset->name, ".exe")
-                && preg_match('/Installer(.*?)\.exe/i', $asset->name, $matches)
+                && preg_match('/-([^-.]+)\.exe/i', $asset->name, $matches)
               ){ // Windows 10, 11
                 $os = "windows";
                 $arch = $matches[1] ? $matches[1] : "unrecognized"; # TODO: UPDATE THIS!
@@ -49,7 +90,7 @@
                 $arch = "unknown";
                 $os = "unknown";
                 $OK = FALSE;
-                echo "ERROR: Unknown installer: " . $asset->name;
+                echo "ERROR: Unknown installer: " . $asset->name . "\n";
               }
               
               // Push to installers
@@ -63,7 +104,7 @@
 
             } else {
               $OK = FALSE;
-              echo "ERROR: There was an issue with downloading the asset.";
+              echo "ERROR: There was an issue with downloading the asset." . "\n";
             }
 
           }
@@ -72,18 +113,18 @@
         }
       } else {
         $OK = FALSE;
-        echo "ERROR: Incorrect hash.";
+        echo "ERROR: Incorrect hash." . "\n";
       }
       
     // } else if (false){
     //   # Do auto deployment here later
     } else {
       $OK = FALSE;
-      echo "ERROR: Unhandled or unrecognized event.";
+      echo "ERROR: Unhandled or unrecognized event." . "\n";
     }
   } else {
     $OK = FALSE;
-    echo "Unable to read webhook payload.";
+    echo "Unable to read webhook payload." . "\n";
   }
 
   if($OK){
@@ -92,39 +133,5 @@
     http_response_code(404); // Bad request
   }
 
-  function downloadAsset($asset){
-    // Download from github using the api endpoint
-    $token = GH_API_TOKEN;
-    $headers = [
-      "X-GitHub-Api-Version: 2022-11-28",
-      "Authorization: ${token}",
-      "User-Agent: MyGitHubClient" // GitHub API requires a User-Agent header
-    ];
-    $ch = curl_init($asset->url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-    $response = curl_exec($ch);
-
-    if (curl_errno($ch)) {
-      $OK = FALSE;
-      $errorMessage = date('Y-m-d H:i:s') . " - cURL Error: " . curl_error($ch) . "\n";
-      file_put_contents("logs.txt", "ERROR: " . $errorMessage, FILE_APPEND);
-      curl_close($ch);
-    } else {
-      $assetPath = "/installers/" . $asset->name;
-      // Put file
-      file_put_contents("." . $assetPath, $response); 
-
-      // Get sha1 sum
-      $sha1Value = hash_file('sha1', "." . $assetPath);
-
-      curl_close($ch);
-      return [
-        'assetPath' => $assetPath,
-        'sha1' => $sha1Value
-      ];
-    }
-  }
 ?>
