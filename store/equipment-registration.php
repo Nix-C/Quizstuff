e6b292daf1c182cf185340e88940cc16abef6633
Michael Parenti <mparenti677@gmail.com>
2025-06-26 16:15:20 -0500

    rm laptop

--- store/equipment-registration.php
+++ store/equipment-registration.php
@@ -34,26 +34,19 @@
         $extension_length = int_or_null($_POST['extension_length']);
         $mic_qty = int_or_null($_POST['mic_qty']);
         $other_qty = int_or_null($_POST['other_qty']);
-        // Insert into equipment_details table (excluding interface_type/interface_qty and pads)
+        // Insert into equipment_details table (excluding interface_type/interface_qty, pads, and laptops)
         $stmt2 = $conn->prepare("INSERT INTO equipment_details (
           registration_id,
-          laptop_brand, laptop_os, laptop_parallel_port, laptop_qm_version, laptop_username, laptop_password,
           monitor_brand, monitor_size, monitor_resolution, monitor_connection,
           projector_brand, projector_lumens, projector_resolution, projector_qty,
           powerstrip_make, powerstrip_model, powerstrip_color, powerstrip_outlets,
           extension_color, extension_length,
           mic_type, mic_brand, mic_model, mic_qty,
           other_desc, other_qty
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" );
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" );
         $stmt2->bind_param(
-          'issssssssssissssssisssssisi',
+          'isssissssssissssisssi',
           $registration_id,
-          $_POST['laptop_brand'],
-          $_POST['laptop_os'],
-          $_POST['laptop_parallel_port'],
-          $_POST['laptop_qm_version'],
-          $_POST['laptop_username'],
-          $_POST['laptop_password'],
           $_POST['monitor_brand'],
           $_POST['monitor_size'],
           $_POST['monitor_resolution'],
