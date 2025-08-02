33ac0c851ab404379fce54b91d62d613a2901da8
Nathaniel Campbell <nixc.web@gmail.com>
2025-08-02 15:55:41 -0500

    use custom sendmail

--- send-help.php
+++ send-help.php
@@ -1,5 +1,13 @@
 <?php
-  require_once 'script.php';
+  use PHPMailer\PHPMailer\Exception;
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\SMTP;
+
+  // Uncomment for local testing
+  require_once 'PHPMailer/src/Exception.php';
+  require_once 'PHPMailer/src/PHPMailer.php';
+  require_once 'PHPMailer/src/SMTP.php';
+  require_once 'config.php';
   $OK = true;
   $emailData = json_decode(file_get_contents('php://input')); 
   file_put_contents('debug.txt', file_get_contents('php://input'));
@@ -12,11 +20,73 @@
     $subject = "Quizstuff Help - ". $emailData->name;
     $res = sendMail($emailData->email, $subject, $emailData->message);
     
-    if($res != "Success.") {
+    if($res) {
       $OK = false;
     }
 
-  // Send a 200 OK HTTP status code
+
+
+    function sendMail($to, $subject, $txt){
+
+      // Create a new mail instance
+      $mail = new \PHPMailer\PHPMailer\PHPMailer();
+
+      // Send using SMTP Protocol
+      $mail->IsSMTP();
+
+      // Authenticate Gmail
+      $mail->SMTPAuth = true;
+
+      // Set Host from config file
+      $mail->Host = MAILHOST;
+
+      // Get Username
+      $mail->Username = USERNAME;
+
+      // Get Password
+      $mail->Password = PASSWORD;
+
+      // Use TTLS encryption
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+      // TCP port number
+      $mail->Port = 587;
+
+      // From
+      $mail->SetFrom(SEND_FROM, SEND_FROM_NAME);
+
+      // To
+      $mail->AddAddress($to);
+      $mail->AddAddress("quizstuff@quizstuff.com");
+
+      // Reply info
+      $mail->addReplyTo(REPLY_TO, REPLY_TO_NAME);
+
+      // Support HTML in email
+      $mail->IsHTML(true);
+
+      // Subject
+      $mail->Subject = $subject;
+
+      // Body
+      $mail->Body = $txt;
+
+      // Body Non-HTML Alternate
+      $mail->AltBody = $txt;
+
+      //Debugging
+      //$mail->SMTPDebug = 3;
+
+      // Send the email
+      if(!$mail->send()){
+          return false;
+      } else {
+          return true;
+      }
+
+    }
+
+      // Send a 200 OK HTTP status code
   if($OK) {
     http_response_code(200);
   } else {
